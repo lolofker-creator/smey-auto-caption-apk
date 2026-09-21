21253cfa62faac3131bc4b28c9ed2474b3b0d96c
lolofker-creator <lolofker@gmail.com>
2026-09-21 22:30:18 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/smeyautocaption/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.smeyautocaption
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl(
+            "https://smey-auto-caption-vip.streamlit.app/"
+        )
+
+        setContentView(webView)
+    }
+}
