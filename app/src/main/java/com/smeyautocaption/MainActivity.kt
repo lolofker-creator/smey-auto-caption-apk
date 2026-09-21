2d5ed1e8e248356a0d1f100d2d2c3b27793e183b
lolofker-creator <lolofker@gmail.com>
2026-09-21 23:45:52 +0700

    Update MainActivity.kt

--- app/src/main/java/com/smeyautocaption/MainActivity.kt
+++ app/src/main/java/com/smeyautocaption/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.smeyautocaption
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
+
+    private var filePathCallback: ValueCallback<Array<Uri>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +22,49 @@ class MainActivity : Activity() {
         webView.settings.domStorageEnabled = true
 
         webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePath: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+
+                filePathCallback?.onReceiveValue(null)
+                filePathCallback = filePath
+
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type = "video/*"
+
+                startActivityForResult(intent, 100)
+                return true
+            }
+        }
 
         webView.loadUrl(
             "https://smey-auto-caption-vip.streamlit.app/"
         )
 
         setContentView(webView)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            val result = if (resultCode == RESULT_OK && data?.data != null) {
+                arrayOf(data.data!!)
+            } else {
+                null
+            }
+
+            filePathCallback?.onReceiveValue(result)
+            filePathCallback = null
+        }
     }
 }
