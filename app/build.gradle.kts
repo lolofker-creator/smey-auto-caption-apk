3a86c443dac75751a6f4bb06dcfc7275414308f2
lolofker-creator <lolofker@gmail.com>
2026-09-21 23:07:53 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,11 +15,3 @@ android {
         versionName = "1.0"
     }
 }
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlinOptions {
-    jvmTarget = "17"
-}
